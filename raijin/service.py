39d889d281c3cc415d570d7641dd2b90741d9700
Rubén de Celis Hernández <contact@rdch106.hol.es>
2018-02-08 23:42:07 +0100

    Add service

---
+++ raijin/service.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# 1) Linkero Core
+import linkero.core.linkero as linkero
+import linkero.core.gateway.gevent_service as gevent
+#import core.gateway.waitress_service as waitress
+
+# 2) APIs developed to use with Linkero
+import api
+
+# 3) Load desired APIs
+api.loadRaijinAPI()
+
+# 4) Run Linkero
+linkero.run()
+#gevent.run(linkero.app)
+#waitress.run(linkero.app)
