7e4f07fd4dc485d1c3793e421e92c900da98cd58
Rubén de Celis Hernández <contact@rdch106.hol.es>
2018-02-08 23:34:27 +0100

    Move bcolors to raijin and delete common

--- raijin/raijin.py
+++ raijin/raijin.py
@@ -8,7 +8,15 @@ import datetime
 import re
 import calendar
 
-from .common import bcolors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 #logging.basicConfig(level=logging.DEBUG)
 logging.basicConfig(level=logging.INFO)
